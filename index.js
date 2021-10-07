974f3e8a45485944ac10fc566adc1ff331bbdfe2
matt594 <matt@dzined.com>
2021-10-07 07:44:39 +0100

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Badge from './components/Badge';
-import Button from './components/Button';
+import Badge from './lib/components/Badge';
+import Button from './lib/components/Button';
 export { Badge, Button };
 ReactDOM.render(
 	<React.StrictMode>
